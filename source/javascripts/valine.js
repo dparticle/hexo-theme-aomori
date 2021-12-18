eff0d6aab9109a4ae5402ed0e8e87d9aeab6ecb2
dparticle <dparticle@163.com>
2021-12-18 13:15:13 +0800

    feat: add deian && valine settings

--- source/javascripts/valine.js
+++ source/javascripts/valine.js
@@ -4,6 +4,9 @@
             el: '#valine-container',
             appId: window.aomori.valine.appId,
             appKey: window.aomori.valine.appKey,
+            placeholder: window.aomori.valine.placeholder,
+            avatar: window.aomori.valine.avatar,
+            pageSize: window.aomori.valine.pageSize,
         })
     }
 })()
